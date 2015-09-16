8fdfb466d786cb8f0653099e4799e13e0773b5df
pezholio <pezholio@gmail.com>
2015-09-16 16:59:35 +0100

    Add update_in_github method to dataset file

--- app/models/dataset_file.rb
+++ app/models/dataset_file.rb
@@ -24,4 +24,12 @@ class DatasetFile < ActiveRecord::Base
       save
     end
 
+    def update_in_github(tempfile)
+      response = dataset.update_contents(filename, tempfile.read, "data", file_sha)
+      self.file_sha = response[:content][:sha]
+      response = dataset.update_contents("#{File.basename(filename, '.*')}.md", File.open(File.join(Rails.root, "extra", "html", "data_view.md")).read, "data", view_sha)
+      self.view_sha = response[:content][:sha]
+      save
+    end
+
 end
--- spec/models/dataset_file_spec.rb
+++ spec/models/dataset_file_spec.rb
@@ -15,31 +15,59 @@ describe DatasetFile do
     expect(file.gh_pages_url).to eq("http://user-mcuser.github.io/my-repo/data/example.csv")
   end
 
-  it "adds a file to Github" do
-    file = build(:dataset_file, filename: "example.csv")
-    file.tempfile = Rack::Test::UploadedFile.new(@path, "text/csv")
+  context "add_to_github" do
 
-    dataset = create(:dataset, repo: "my-repo", user: @user, dataset_files: [file])
+    before(:each) do
+      @file = build(:dataset_file, filename: "example.csv")
+      @file.tempfile = Rack::Test::UploadedFile.new(@path, "text/csv")
 
-    expect(dataset).to receive(:create_contents).with("example.csv", File.read(@path), "data") { { content: {} } }
-    expect(dataset).to receive(:create_contents).with("example.md", File.open(File.join(Rails.root, "extra", "html", "data_view.md")).read, "data") { { content: {} } }
+      @dataset = create(:dataset, repo: "my-repo", user: @user, dataset_files: [@file])
+    end
 
-    file.send(:add_to_github)
+    it "adds a file to Github" do
+      expect(@dataset).to receive(:create_contents).with("example.csv", File.read(@path), "data") { { content: {} } }
+      expect(@dataset).to receive(:create_contents).with("example.md", File.open(File.join(Rails.root, "extra", "html", "data_view.md")).read, "data") { { content: {} } }
+
+      @file.send(:add_to_github)
+    end
+
+    it "sets the sha", :vcr do
+      expect(@dataset).to receive(:create_contents).with("example.csv", File.read(@path), "data") { { content: { sha: "sha1"} } }
+      expect(@dataset).to receive(:create_contents).with("example.md", File.open(File.join(Rails.root, "extra", "html", "data_view.md")).read, "data") { { content: { sha: "sha2"} } }
+
+      @file.send(:add_to_github)
+
+      expect(@file.file_sha).to eq("sha1")
+      expect(@file.view_sha).to eq("sha2")
+    end
   end
 
-  it "sets the sha after adding to Github", :vcr do
-    file = build(:dataset_file, filename: "example.csv")
-    file.tempfile = Rack::Test::UploadedFile.new(@path, "text/csv")
+  context "update_in_github" do
 
-    dataset = create(:dataset, repo: "my-repo", user: @user, dataset_files: [file])
+    before(:each) do
+      @file = create(:dataset_file, filename: "example.csv", file_sha: "abbsdfsdfsdfsdvs", view_sha: "fgdfdgdfgdfgf")
+      @tempfile = Rack::Test::UploadedFile.new(@path, "text/csv")
 
-    expect(dataset).to receive(:create_contents).with("example.csv", File.read(@path), "data") { { content: { sha: "sha1"} } }
-    expect(dataset).to receive(:create_contents).with("example.md", File.open(File.join(Rails.root, "extra", "html", "data_view.md")).read, "data") { { content: { sha: "sha2"} } }
+      @dataset = create(:dataset, repo: "my-repo", user: @user, dataset_files: [@file])
+    end
 
-    file.send(:add_to_github)
+    it "updates a file in Github" do
+      expect(@dataset).to receive(:update_contents).with("example.csv", File.read(@path), "data", @file.file_sha) { { content: {} } }
+      expect(@dataset).to receive(:update_contents).with("example.md", File.open(File.join(Rails.root, "extra", "html", "data_view.md")).read, "data", @file.view_sha) { { content: {} } }
 
-    expect(file.file_sha).to eq("sha1")
-    expect(file.view_sha).to eq("sha2")
+      @file.send(:update_in_github, @tempfile)
+    end
+
+    it "sets the new sha" do
+      expect(@dataset).to receive(:update_contents).with("example.csv", File.read(@path), "data", @file.file_sha) { { content: { sha: "sha1"} } }
+      expect(@dataset).to receive(:update_contents).with("example.md", File.open(File.join(Rails.root, "extra", "html", "data_view.md")).read, "data", @file.view_sha) { { content: { sha: "sha2"} } }
+
+      @file.send(:update_in_github, @tempfile)
+
+      expect(@file.file_sha).to eq("sha1")
+      expect(@file.view_sha).to eq("sha2")
+    end
+
   end
 
 end
