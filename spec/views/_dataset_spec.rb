6fc23b0a8cd622303ea35b75f25c2f3eb65338ae
pezholio <pezholio@gmail.com>
2015-09-18 16:24:22 +0100

    Extra view tests

--- spec/views/_dataset_spec.rb
+++ spec/views/_dataset_spec.rb
@@ -2,13 +2,25 @@ require 'spec_helper'
 
 describe 'datasets/_dataset.html.erb' do
 
+  before(:each) do
+    @user = create(:user, name: "user")
+    @dataset = create(:dataset, name: "My Dataset", repo: "my-repo", user: @user)
+  end
+
   it 'displays a single dataset' do
-    user = create(:user, name: "user")
-    dataset = create(:dataset, name: "My Dataset", repo: "my-repo", user: user)
+    render :partial => 'datasets/dataset.html.erb', :locals => {:dataset => @dataset}
+    expect(rendered).to match /<a href="http:\/\/user.github.io\/my-repo">My Dataset<\/a>/
+  end
 
-    render :partial => 'datasets/dataset.html.erb', :locals => {:dataset => dataset}
+  it 'does not display the edit link when path is not the dashboard' do
+    render :partial => 'datasets/dataset.html.erb', :locals => {:dataset => @dataset}
+    expect(rendered).to_not match /Edit/
+  end
 
-    expect(rendered).to match /<li><a href="http:\/\/user.github.io\/my-repo">My Dataset<\/a><\/li>/
+  it 'displays the edit link when in the dashboard' do
+    controller.request.env['PATH_INFO'] = "/dashboard"
+    render :partial => 'datasets/dataset.html.erb', :locals => {:dataset => @dataset}
+    expect(rendered).to match /<a class=\"btn btn-xs btn-warning\" href=\"\/datasets\/#{@dataset.id}\/edit\">Edit<\/a>/
   end
 
 end
