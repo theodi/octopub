df227ef4ca4279b1228747c762f70a600f168ae5
pezholio <pezholio@gmail.com>
2016-05-10 15:08:59 +0100

    No new episodes
    WHAT DOES IT EVEN MEEEEEAAAANNNNNNN?

--- spec/support/vcr_helper.rb
+++ spec/support/vcr_helper.rb
@@ -3,7 +3,7 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/cassettes'
   c.hook_into :webmock
-  c.default_cassette_options = { record: :new_episodes }
+  c.default_cassette_options = { record: :once }
   c.configure_rspec_metadata!
   c.allow_http_connections_when_no_cassette = true
   c.filter_sensitive_data("<GITHUB_TOKEN>") { ENV['GITHUB_TOKEN'] }
