e43ad42b3d01a6831fc37bb43032795e50ce1c57
pezholio <pezholio@gmail.com>
2016-05-12 09:38:12 +0100

    Return API key as JSON if requested
    Plus added controller tests for SessionsController!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,29 @@
 class SessionsController < ApplicationController
   def create
-    auth = request.env["omniauth.auth"]
     user = User.find_for_github_oauth(auth)
-    session[:user_id] = user.id
 
-    redirect_to root_url, :notice => "Signed in!"
+    if format == "json"
+      render json: {
+        api_key: user.api_key
+      }.to_json
+    else
+      session[:user_id] = user.id
+      redirect_to root_url, :notice => "Signed in!"
+    end
   end
 
   def destroy
     session[:user_id] = nil
-
     redirect_to root_url, :notice => "Signed out!"
   end
 
+  private
+
+    def auth
+      request.env["omniauth.auth"]
+    end
+
+    def format
+      (request.env["omniauth.params"] || {})["format"]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'application#index'
 
-  get "/auth/:provider/callback" => "sessions#create"
+  get "/auth/:provider/callback" => "sessions#create", :as => :callback
   get "/signout" => "sessions#destroy", :as => :signout
 
   resources :datasets
