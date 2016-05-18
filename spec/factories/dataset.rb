dbd826d12c62d76a1f65db5b5d2f78d1de97a800
pezholio <pezholio@gmail.com>
2016-05-18 13:45:55 +0100

    Test fixes

--- spec/factories/dataset.rb
+++ spec/factories/dataset.rb
@@ -11,12 +11,15 @@ FactoryGirl.define do
 
     after(:build) { |dataset|
       dataset.class.skip_callback(:create, :after, :create_in_github)
+      dataset.class.skip_callback(:update, :after, :update_in_github)
+
       dataset.instance_variable_set(:@repo, FakeData.new)
     }
 
     trait :with_callback do
       after(:build) { |dataset|
         dataset.class.set_callback(:create, :after, :create_in_github)
+        dataset.class.set_callback(:update, :after, :update_in_github)
       }
     end
   end
--- spec/models/dataset_file_spec.rb
+++ spec/models/dataset_file_spec.rb
@@ -36,8 +36,8 @@ describe DatasetFile do
   context "update_in_github" do
 
     before(:each) do
-      @file = create(:dataset_file, filename: "example.csv")
       @tempfile = Rack::Test::UploadedFile.new(@path, "text/csv")
+      @file = create(:dataset_file, filename: "example.csv", file: @tempfile)
 
       @dataset = create(:dataset, repo: "my-repo", user: @user, dataset_files: [@file])
     end
@@ -46,7 +46,7 @@ describe DatasetFile do
       expect(@dataset).to receive(:update_contents).with("data/example.csv", File.read(@path))
       expect(@dataset).to receive(:update_contents).with("data/example.md", File.open(File.join(Rails.root, "extra", "html", "data_view.md")).read)
 
-      @file.send(:update_in_github, @tempfile)
+      @file.send(:update_in_github)
     end
   end
 
@@ -93,7 +93,7 @@ describe DatasetFile do
     it "updates a file" do
       file = create(:dataset_file, filename: 'test-data.csv')
 
-      path = File.join(Rails.root, 'spec', 'fixtures', 'test-data.csv')
+      path = File.join(Rails.root, 'spec', 'fixtures', 'test-data0.csv')
       tempfile = Rack::Test::UploadedFile.new(path, "text/csv")
 
       new_file = {
@@ -103,11 +103,10 @@ describe DatasetFile do
         "description" => 'A new description',
       }
 
-      expect(file).to receive(:update_in_github).with(tempfile)
       file.update_file(new_file)
 
       expect(file.title).to eq(new_file["title"])
-      expect(file.filename).to eq(tempfile.original_filename)
+      expect(file.filename).to eq('test-data.csv')
       expect(file.description).to eq(new_file["description"])
       expect(file.mediatype).to eq("text/csv")
     end
@@ -127,24 +126,6 @@ describe DatasetFile do
       expect(file.title).to eq(new_file["title"])
       expect(file.description).to eq(new_file["description"])
       expect(file.mediatype).to eq("text/csv")
-    end
-
-    it "deletes the old file if the filenames are different" do
-      file = create(:dataset_file)
-
-      path = File.join(Rails.root, 'spec', 'fixtures', 'test-data0.csv')
-      tempfile = Rack::Test::UploadedFile.new(path, "text/csv")
-
-      new_file = {
-        "id" => file.id,
-        "title" => 'My File',
-        "file" => tempfile,
-        "description" => 'A new description',
-      }
-
-      expect(file).to receive(:update_in_github).with(tempfile)
-      expect(file).to receive(:delete_from_github)
-      file.update_file(new_file)
     end
 
   end
