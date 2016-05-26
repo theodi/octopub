8da68c70e901dec082127dc675db2e6d40c9ff62
pezholio <pezholio@gmail.com>
2016-05-26 08:44:23 +0100

    Add organisation select to form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+
+  def organization_options
+    current_user.organizations.collect { |o|
+      [
+        o.organization.login,
+        o.organization.login,
+        { 'data-content' => "<img src='#{o.organization.avatar_url}' height='20' width='20' /> #{o.organization.login}" }
+      ]
+    }
+  end
+
+  def user_option
+    [
+      current_user.github_username,
+      current_user.github_username,
+      { 'data-content' => "<img src='#{current_user.github_user.avatar_url}' height='20' width='20' /> #{current_user.github_username}" }
+    ]
+  end
+
+  def organization_select_options
+    organization_options.unshift(user_option)
+  end
+
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,99 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+
+  before(:each) do
+    @user = create(:user)
+
+    allow(helper).to receive(:current_user) {
+      @user
+    }
+
+    allow(@user).to receive(:organizations) {
+      [
+        OpenStruct.new(
+          organization: OpenStruct.new({
+            login: "org1",
+            avatar_url: "http://www.example.org/avatar1.png"
+          })
+        ),
+        OpenStruct.new(
+          organization: OpenStruct.new({
+            login: "org2",
+            avatar_url: "http://www.example.org/avatar2.png"
+          })
+        ),
+        OpenStruct.new(
+          organization: OpenStruct.new({
+            login: "org3",
+            avatar_url: "http://www.example.org/avatar3.png"
+          })
+        )
+      ]
+    }
+
+    allow(@user).to receive(:github_user) {
+      OpenStruct.new(
+        avatar_url: "http://www.example.org/avatar2.png"
+      )
+    }
+  end
+
+  it 'gets organization options' do
+    expect(helper.organization_options).to eq(
+      [
+        [
+          'org1',
+          'org1',
+          { 'data-content' => "<img src='http://www.example.org/avatar1.png' height='20' width='20' /> org1" }
+        ],
+        [
+          'org2',
+          'org2',
+          { 'data-content' => "<img src='http://www.example.org/avatar2.png' height='20' width='20' /> org2" }
+        ],
+        [
+          'org3',
+          'org3',
+          { 'data-content' => "<img src='http://www.example.org/avatar3.png' height='20' width='20' /> org3" }
+        ]
+      ]
+    )
+  end
+
+  it 'gets user option' do
+    expect(helper.user_option).to eq(
+      [
+        'user',
+        'user',
+        { 'data-content' => "<img src='http://www.example.org/avatar2.png' height='20' width='20' /> user" }
+      ]
+    )
+  end
+
+  it 'gets all options' do
+    expect(helper.organization_select_options).to eq([
+      [
+        'user',
+        'user',
+        { 'data-content' => "<img src='http://www.example.org/avatar2.png' height='20' width='20' /> user" }
+      ],
+      [
+        'org1',
+        'org1',
+        { 'data-content' => "<img src='http://www.example.org/avatar1.png' height='20' width='20' /> org1" }
+      ],
+      [
+        'org2',
+        'org2',
+        { 'data-content' => "<img src='http://www.example.org/avatar2.png' height='20' width='20' /> org2" }
+      ],
+      [
+        'org3',
+        'org3',
+        { 'data-content' => "<img src='http://www.example.org/avatar3.png' height='20' width='20' /> org3" }
+      ]
+    ])
+  end
+
+end
