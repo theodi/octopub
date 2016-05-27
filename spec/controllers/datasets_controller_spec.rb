aa4f588b5baf1602bc1a111cf37791b5d2324d46
pezholio <pezholio@gmail.com>
2016-05-27 16:00:17 +0100

    Test JSON response

--- spec/controllers/datasets_controller_spec.rb
+++ spec/controllers/datasets_controller_spec.rb
@@ -26,6 +26,14 @@ describe DatasetsController, type: :controller do
       get 'index'
       expect(assigns(:datasets).count).to eq(5)
     end
+
+    it "lists the json" do
+      5.times { |i| create(:dataset, name: "Dataset #{i}") }
+      get 'index', format: :json
+
+      expect(assigns(:datasets).count).to eq(5)
+      expect(response.content_type).to eq("application/json") 
+    end
   end
 
   describe 'dashboard' do
