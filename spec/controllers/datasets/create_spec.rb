15c216df00738f492698b4a00b5f82b7a02e73ec
pezholio <pezholio@gmail.com>
2016-06-16 07:29:59 +0100

    Split dataset controller tests up

---
+++ spec/controllers/datasets/create_spec.rb
@@ -0,0 +1,327 @@
+require 'spec_helper'
+
+describe DatasetsController, type: :controller do
+
+  before(:each) do
+    @user = create(:user, name: "User McUser", email: "user@user.com")
+    sign_in @user
+
+    @name = "My cool dataset"
+    @description = "This is a description"
+    @publisher_name = "Cool inc"
+    @publisher_url = "http://example.com"
+    @license = "OGL-UK-3.0"
+    @frequency = "Monthly"
+    @files ||= []
+
+    Dataset.skip_callback(:create, :after, :create_in_github)
+    Dataset.skip_callback(:create, :after, :set_owner_avatar)
+
+    allow_any_instance_of(DatasetFile).to receive(:add_to_github) { nil }
+    allow_any_instance_of(Dataset).to receive(:create_files) { nil }
+  end
+
+  after(:each) do
+    Dataset.set_callback(:create, :after, :create_in_github)
+    Dataset.set_callback(:create, :after, :set_owner_avatar)
+  end
+
+  describe 'create dataset' do
+
+    it 'returns an error if there are no files specified' do
+      request = post 'create', dataset: {
+        name: @name,
+        description: @description,
+        publisher_name: @publisher_name,
+        publisher_url: @publisher_url,
+        license: @license,
+        frequency: @frequency
+      }, files: []
+
+      expect(request).to render_template(:new)
+      expect(flash[:notice]).to eq("You must specify at least one dataset")
+    end
+
+    context 'with one file' do
+
+      before(:each) do
+        name = 'Test Data'
+        description = Faker::Company.bs
+        filename = 'test-data.csv'
+        path = File.join(Rails.root, 'spec', 'fixtures', filename)
+
+        Dataset.set_callback(:create, :after, :create_in_github)
+
+        @files << {
+          :title => name,
+          :description => description,
+          :file => fake_file(path)
+        }
+
+        @repo = double(GitData)
+
+        expect(@repo).to receive(:html_url) { nil }
+        expect(@repo).to receive(:name) { nil }
+        expect(@repo).to receive(:save)
+      end
+
+      it 'creates a dataset with one file' do
+        expect(GitData).to receive(:create).with(@user.github_username, @name, client: a_kind_of(Octokit::Client)) {
+          @repo
+        }
+
+        request = post 'create', dataset: {
+          name: @name,
+          description: @description,
+          publisher_name: @publisher_name,
+          publisher_url: @publisher_url,
+          license: @license,
+          frequency: @frequency
+        }, files: @files
+
+        expect(request).to redirect_to(dashboard_path)
+        expect(flash[:notice]).to eq("Dataset created sucessfully")
+        expect(Dataset.count).to eq(1)
+        expect(@user.datasets.count).to eq(1)
+        expect(@user.datasets.first.dataset_files.count).to eq(1)
+      end
+
+      it 'creates a dataset in an organization' do
+        organization = 'my-cool-organization'
+
+        expect(GitData).to receive(:create).with(organization, @name, client: a_kind_of(Octokit::Client)) {
+          @repo
+        }
+
+        request = post 'create', dataset: {
+          name: @name,
+          description: @description,
+          publisher_name: @publisher_name,
+          publisher_url: @publisher_url,
+          license: @license,
+          frequency: @frequency,
+          owner: organization
+        }, files: @files
+
+        expect(request).to redirect_to(dashboard_path)
+        expect(flash[:notice]).to eq("Dataset created sucessfully")
+        expect(Dataset.count).to eq(1)
+        expect(@user.datasets.count).to eq(1)
+        expect(@user.datasets.first.dataset_files.count).to eq(1)
+      end
+
+    end
+
+    context('with a schema') do
+
+      before(:each) do
+        schema_path = File.join(Rails.root, 'spec', 'fixtures', 'schemas', 'good-schema.json')
+        @schema = Rack::Test::UploadedFile.new(schema_path, "text/csv")
+      end
+
+      it 'returns an error if the file does not match the schema' do
+        path = File.join(Rails.root, 'spec', 'fixtures', 'invalid-schema.csv')
+        schema_path = File.join(Rails.root, 'spec', 'fixtures', 'schemas', 'good-schema.json')
+
+        @files << {
+          :title => 'My File',
+          :description => 'My Description',
+          :file => fake_file(path)
+        }
+
+        request = post 'create', dataset: {
+          name: @name,
+          description: @description,
+          publisher_name: @publisher_name,
+          publisher_url: @publisher_url,
+          license: @license,
+          frequency: @frequency,
+          schema: @schema
+        }, files: @files
+
+        expect(Dataset.count).to eq(0)
+        expect(request).to render_template(:new)
+        expect(flash[:notice]).to eq("Your file 'My File' does not match the schema you provided")
+      end
+
+      it 'creates sucessfully if the file matches the schema' do
+        path = File.join(Rails.root, 'spec', 'fixtures', 'valid-schema.csv')
+
+        @files << {
+          :title => 'My File',
+          :description => 'My Description',
+          :file => fake_file(path)
+        }
+
+        request = post 'create', dataset: {
+          name: @name,
+          description: @description,
+          publisher_name: @publisher_name,
+          publisher_url: @publisher_url,
+          license: @license,
+          frequency: @frequency,
+          schema: @schema
+        }, files: @files
+
+        expect(request).to redirect_to(dashboard_path)
+        expect(flash[:notice]).to eq("Dataset created sucessfully")
+        expect(Dataset.count).to eq(1)
+        expect(@user.datasets.count).to eq(1)
+        expect(@user.datasets.first.dataset_files.count).to eq(1)
+      end
+
+    end
+
+    context 'via the API' do
+
+      before(:each) do
+        name = 'Test Data'
+        description = Faker::Company.bs
+        filename = 'test-data.csv'
+        path = File.join(Rails.root, 'spec', 'fixtures', filename)
+
+        Dataset.set_callback(:create, :after, :create_in_github)
+
+        @repo = double(GitData)
+
+        allow(GitData).to receive(:create).with(@user.github_username, @name, client: a_kind_of(Octokit::Client)) {
+          @repo
+        }
+
+        @files << {
+          :title => name,
+          :description => description,
+          :file => Rack::Test::UploadedFile.new(path, "text/csv")
+        }
+      end
+
+      it 'creates a dataset with JSON' do
+        expect(@repo).to receive(:html_url) { 'https://github.com/user-mc-user/my-cool-repo' }
+        expect(@repo).to receive(:name) { 'my-cool-repo' }
+        expect(@repo).to receive(:save)
+
+        post 'create', :format => :json, dataset: {
+          name: @name,
+          description: @description,
+          publisher_name: @publisher_name,
+          publisher_url: @publisher_url,
+          license: @license,
+          frequency: @frequency
+        },
+        files: @files,
+        api_key: @user.api_key
+
+        expect(Dataset.count).to eq(1)
+        expect(@user.datasets.count).to eq(1)
+        expect(@user.datasets.first.dataset_files.count).to eq(1)
+
+        expect(response.body).to eq({
+          "id": Dataset.first.id,
+          "name":"My cool dataset",
+          "url": "https://github.com/user-mc-user/my-cool-repo",
+          "user_id":@user.id,
+          "created_at": Dataset.first.created_at,
+          "updated_at": Dataset.first.updated_at,
+          "repo":"my-cool-repo",
+          "description":"This is a description",
+          "publisher_name":"Cool inc",
+          "publisher_url":"http://example.com",
+          "license":"OGL-UK-3.0",
+          "frequency":"Monthly",
+          "datapackage_sha": nil,
+          "owner": nil,
+          "owner_avatar": nil,
+          "gh_pages_url":"http://user-mcuser.github.io/my-cool-repo"
+        }.to_json)
+      end
+
+      context('with a schema') do
+
+        before(:each) do
+          schema_path = File.join(Rails.root, 'spec', 'fixtures', 'schemas', 'good-schema.json')
+          @schema = Rack::Test::UploadedFile.new(schema_path, "text/csv")
+        end
+
+        it 'creates a dataset sucessfully' do
+          expect(@repo).to receive(:html_url) { 'https://github.com/user-mc-user/my-cool-repo' }
+          expect(@repo).to receive(:name) { 'my-cool-repo' }
+          expect(@repo).to receive(:save)
+
+          path = File.join(Rails.root, 'spec', 'fixtures', 'valid-schema.csv')
+
+          files = [{
+            :title => 'My File',
+            :description => 'My Description',
+            :file => Rack::Test::UploadedFile.new(path, "text/csv")
+          }]
+
+          post 'create', :format => :json, dataset: {
+            name: @name,
+            description: @description,
+            publisher_name: @publisher_name,
+            publisher_url: @publisher_url,
+            license: @license,
+            frequency: @frequency,
+            schema: @schema
+          },
+          files: files,
+          api_key: @user.api_key
+
+          expect(Dataset.count).to eq(1)
+          expect(@user.datasets.count).to eq(1)
+          expect(@user.datasets.first.dataset_files.count).to eq(1)
+        end
+
+        it 'errors is a file does not match the schema' do
+          path = File.join(Rails.root, 'spec', 'fixtures', 'invalid-schema.csv')
+
+          files = [{
+            :title => 'My File',
+            :description => 'My Description',
+            :file => Rack::Test::UploadedFile.new(path, "text/csv")
+          }]
+
+          post 'create', :format => :json, dataset: {
+            name: @name,
+            description: @description,
+            publisher_name: @publisher_name,
+            publisher_url: @publisher_url,
+            license: @license,
+            frequency: @frequency,
+            schema: @schema
+          },
+          files: files,
+          api_key: @user.api_key
+
+          expect(Dataset.count).to eq(0)
+          expect(response.body).to eq({
+            "errors": [
+              "Your file 'My File' does not match the schema you provided"
+            ]
+          }.to_json)
+        end
+
+      end
+
+      it 'skips the authenticity token if creating via the API' do
+        expect(@repo).to receive(:html_url) { 'https://github.com/user-mc-user/my-cool-repo' }
+        expect(@repo).to receive(:name) { 'my-cool-repo' }
+        expect(@repo).to receive(:save)
+        expect(controller).to_not receive(:verify_authenticity_token)
+
+        post 'create', :format => :json, dataset: {
+          name: @name,
+          description: @description,
+          publisher_name: @publisher_name,
+          publisher_url: @publisher_url,
+          license: @license,
+          frequency: @frequency
+        },
+        files: @files,
+        api_key: @user.api_key
+      end
+
+    end
+
+  end
+end
---
+++ spec/controllers/datasets/dashboard_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe DatasetsController, type: :controller do
+
+  before(:each) do
+    @user = create(:user, name: "User McUser", email: "user@user.com")
+  end
+
+  describe 'dashboard' do
+    it "gets the right number of datasets" do
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:github]
+
+      5.times { |i| create(:dataset, name: "Dataset #{i}") }
+
+      create(:dataset, user: @user)
+      sign_in @user
+
+      get 'dashboard'
+
+      expect(assigns(:datasets).count).to eq(1)
+    end
+
+    it "refreshes datasets" do
+      # This dataset exists
+      dataset1 = create(:dataset, user: @user, repo: "dataset-1")
+      allow_any_instance_of(Octokit::Client).to receive(:repository).with(dataset1.full_name)
+
+      # This dataset has gone away
+      dataset2 = create(:dataset, user: @user, repo: "dataset-2")
+      allow_any_instance_of(Octokit::Client).to receive(:repository).with(dataset2.full_name) { raise Octokit::NotFound }
+
+      sign_in @user
+
+      get 'dashboard', refresh: true
+
+      expect(assigns(:datasets).count).to eq(1)
+      expect(assigns(:datasets).first).to eq(dataset1)
+    end
+
+    it 'gets a JSON dashboard' do
+      5.times { |i| create(:dataset, name: "Dataset #{i}") }
+
+      dataset = create(:dataset, user: @user)
+      get 'dashboard', format: :json, api_key: @user.api_key
+
+      json = JSON.parse(response.body)
+
+      expect(json['datasets'].count).to eq(1)
+      expect(json['datasets'].first['name']).to eq(dataset.name)
+    end
+  end
+end
