de29a8d84fd2cc42c5c33443d1123350454e940c
pezholio <pezholio@gmail.com>
2016-06-22 14:38:24 +0100

    Set all existing datasets to have a build status of "built"

--- db/migrate/20160622094152_add_build_status_to_dataset.rb
+++ db/migrate/20160622094152_add_build_status_to_dataset.rb
@@ -1,5 +1,6 @@
 class AddBuildStatusToDataset < ActiveRecord::Migration
   def change
     add_column :datasets, :build_status, :string
+    Dataset.all.each { |d| d.build_status == "built" }
   end
 end
