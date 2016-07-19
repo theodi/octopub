336fdc18288038b47ef5d8323f33d83c1375d32b
pezholio <pezholio@gmail.com>
2016-07-19 16:21:52 +0100

    Add Mandrill config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: "octopub.io" }
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mandrillapp.com",
+    port: 25,
+    enable_starttls_auto: true, #
+    user_name: ENV["MANDRILL_USERNAME"],
+    password: ENV["MANDRILL_PASSWORD"],
+    authentication: 'login'
+  }
 end
