bdade455054151d2b064390398f7d67b22759515
pikesley <github@orgraphone.org>
2016-07-19 15:12:49 +0100

    Build cert and send success email

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -7,7 +7,7 @@ class Dataset < ActiveRecord::Base
   belongs_to :user
   has_many :dataset_files
 
-  after_create :create_in_github, :set_owner_avatar
+  after_create :create_in_github, :set_owner_avatar, :build_certificate, :send_success_email
   after_update :update_in_github
   after_destroy :delete_in_github
 
@@ -17,17 +17,28 @@ class Dataset < ActiveRecord::Base
   validate :check_repo, on: :create
   validates_associated :dataset_files
 
-  def self.create_certificate id
-    dataset = Dataset.find(id)
+  def send_success_email
+    DatasetMailer.success(self).deliver
+  end
 
-    cert = CertificateFactory::Certificate.new dataset.gh_pages_url
+  def build_certificate
+    status = user.octokit_client.pages(full_name).status
+    if status == "built"
+      create_certificate
+    else
+      sleep 5
+      build_certificate
+    end
+  end
+
+  def create_certificate
+    cert = CertificateFactory::Certificate.new gh_pages_url
 
     gen = cert.generate
 
     if gen[:success] == 'pending'
       result = cert.result
-      dataset.add_certificate_url(result[:certificate_url])
-      DatasetMailer.success(dataset).deliver
+      add_certificate_url(result[:certificate_url])
     end
   end
 
