b87059ee3a479d73f23595ed06dc4a1ad709388a
pezholio <pezholio@gmail.com>
2016-07-20 13:37:01 +0100

    Change port number

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,8 +80,8 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     address: "smtp.mandrillapp.com",
-    port: 25,
-    enable_starttls_auto: true, #
+    port: 587,
+    enable_starttls_auto: true,
     user_name: ENV["MANDRILL_USERNAME"],
     password: ENV["MANDRILL_PASSWORD"],
     authentication: 'login'
