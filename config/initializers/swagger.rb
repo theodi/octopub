7aded1a14a693176439b611362ad0508abea820b
pezholio <pezholio@gmail.com>
2016-08-04 18:51:12 +0100

    Allow auth header to be passed to API in test requests

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -2,6 +2,9 @@ GrapeSwaggerRails.options.url      = '/api/swagger_doc'
 GrapeSwaggerRails.options.app_name = 'Octopub'
 GrapeSwaggerRails.options.validator_url = nil
 
+GrapeSwaggerRails.options.api_key_name = 'Authorization'
+GrapeSwaggerRails.options.api_key_type = 'header'
+
 GrapeSwaggerRails.options.before_filter_proc = proc {
   GrapeSwaggerRails.options.app_url = request.protocol + request.host_with_port
 }
