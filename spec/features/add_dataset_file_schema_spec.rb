be66289660e4c0af71ed14853d16e791bf00e5a7
James Jefferies <james@shedcode.co.uk>
2017-02-16 15:52:23 +0000

    Add some tests

--- spec/features/add_dataset_file_schema_spec.rb
+++ spec/features/add_dataset_file_schema_spec.rb
@@ -1,39 +1,10 @@
 require "rails_helper"
+require 'features/user_and_organisations'
 
 feature "Add dataset page", type: :feature, vcr: { :match_requests_on => [:host, :method] } do
+  include_context 'user and organisations'
 
-  let(:organizations) {
-    [
-      OpenStruct.new(
-        organization: OpenStruct.new({
-          login: "org1",
-          avatar_url: "http://www.example.org/avatar1.png"
-        })
-      ),
-      OpenStruct.new(
-        organization: OpenStruct.new({
-          login: "org2",
-          avatar_url: "http://www.example.org/avatar2.png"
-        })
-      ),
-      OpenStruct.new(
-        organization: OpenStruct.new({
-          login: "org3",
-          avatar_url: "http://www.example.org/avatar3.png"
-        })
-      )
-    ]
-  }
-
-  let(:github_user) {
-    OpenStruct.new(
-      avatar_url: "http://www.example.org/avatar2.png"
-    )
-  }
-
-  let(:data_file) {
-    File.join(Rails.root, 'spec', 'fixtures', 'valid-schema.csv')
-  }
+  let(:data_file) {File.join(Rails.root, 'spec', 'fixtures', 'valid-schema.csv') }
 
   before(:each) do
     @user = create(:user, name: "User McUser", email: "user@user.com")
@@ -41,54 +12,69 @@ feature "Add dataset page", type: :feature, vcr: { :match_requests_on => [:host,
     sign_in @user
     allow_any_instance_of(User).to receive(:organizations) { organizations }
     allow_any_instance_of(User).to receive(:github_user) { github_user }
+    allow_any_instance_of(DatasetFileSchemaService).to receive(:read_file_with_utf_8).and_return(File.read(File.join(Rails.root, 'spec', 'fixtures', 'schemas/good-schema.json')))
   end
 
   context "logged in visitors has no schemas" do
     scenario "and can add a dataset file schema on it's own" do
-
       visit root_path
 
-      click_link "List my dataset file schemas"
-      expect(page).to have_content "You currently have no dataset file schemas, why not add one?"
+      click_link 'List my dataset file schemas'
+      expect(page).to have_content 'You currently have no dataset file schemas, why not add one?'
+      click_link 'Add a new dataset file schema'
+      common_name = 'Fri1437'
 
-      
-      # common_name = 'Fri1437'
+      before_datasets = DatasetFileSchema.count
 
-      # before_datasets = Dataset.count
-      # expect(page).to have_selector(:link_or_button, "Submit")
-      # within 'form' do
-      #   expect(page).to have_content "user-mcuser"
-      #   expect(page).to have_content "Upload a schema for this Data File"
-      #   complete_form(page, common_name, data_file)   
-      # end
+      within 'form' do
+        fill_in 'dataset_file_schema_name', with: "#{common_name}-schema-name"
+        fill_in 'dataset_file_schema_description', with: "#{common_name}-schema-description"
+        attach_file('dataset_file_schema_url_in_s3', data_file)
 
-      # # Bypass sidekiq completely
-      # allow(CreateDataset).to receive(:perform_async) do |a,b,c,d|
-      #   CreateDataset.new.perform(a,b,c,d)
-      # end
+        click_on 'Submit'
+      end
 
-      # click_on 'Submit'
-
-      # expect(page).to have_content "Your dataset has been queued for creation, and you should receive an email with a link to your dataset on Github shortly."
-      # expect(Dataset.count).to be before_datasets + 1
-      # expect(Dataset.last.name).to eq "#{common_name}-name"
+      expect(page).to have_content "Dataset File Schemas for #{@user.name}"
+      expect(DatasetFileSchema.count).to be before_datasets + 1
+      expect(DatasetFileSchema.last.name).to eq "#{common_name}-schema-name"
     end
 
-  end
+    context "and gets an error if they do not populate the correct fields" do
 
-  def complete_schema_form(page, common_name, data_file, owner = nil, licence = nil)
+      before(:each) do
+        visit root_path
+        click_link 'List my dataset file schemas'
+        expect(page).to have_content 'You currently have no dataset file schemas, why not add one?'
+        click_link 'Add a new dataset file schema'
+        expect(page).to have_content 'Add a new Dataset File Schema'
+      end
 
-    # dataset_name = "#{common_name}-name"
+      it "errors if no name" do
+        common_name = 'Fri1437'
 
-    # fill_in 'dataset[name]', with: dataset_name
-    # fill_in 'dataset[description]', with: "#{common_name}-description"
-    # fill_in 'dataset[publisher_name]', with: "#{common_name}-publisher-name"
-    # fill_in 'dataset[publisher_url]', with: "http://#{common_name}-publisher-url.example.com/"
-    # fill_in 'files[][title]', with: "#{common_name}-file-name"
-    # fill_in 'files[][description]', with: "#{common_name}-file-description"
-    # attach_file("[files[][file]]", data_file)
-    # expect(page).to have_selector("input[value='#{dataset_name}']")
+        within 'form' do
+          fill_in 'dataset_file_schema_description', with: "#{common_name}-schema-description"
+          attach_file('dataset_file_schema_url_in_s3', data_file)
+          click_on 'Submit'
+        end
 
+        expect(page).to have_content 'Add a new Dataset File Schema'
+        expect(page).to have_content 'Please give the schema a meaningful name'
+      end
+
+      it "errors if no file" do
+        common_name = 'Fri1437'
+
+        within 'form' do
+          fill_in 'dataset_file_schema_name', with: "#{common_name}-schema-name"
+          fill_in 'dataset_file_schema_description', with: "#{common_name}-schema-description"
+          click_on 'Submit'
+        end
+
+        expect(page).to have_content 'Add a new Dataset File Schema'
+        expect(page).to have_content 'You must have a schema file'
+      end
+    end
   end
 end
 
