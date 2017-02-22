ea00026beb937776501446ab90aa16c5d27a3e09
James Jefferies <james@shedcode.co.uk>
2017-02-22 12:36:17 +0000

    Delete session user rather than set it to nil

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    session.delete(:user_id)
     redirect_to root_url, :notice => "Signed out!"
   end
 
