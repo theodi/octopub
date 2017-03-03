8e63422830baa0799e0861d604a14f3dae0f7d23
James Jefferies <james@shedcode.co.uk>
2017-03-03 10:59:54 +0000

    Tidy commented out stuff and add ordering

--- app/controllers/dataset_file_schemas_controller.rb
+++ app/controllers/dataset_file_schemas_controller.rb
@@ -1,7 +1,7 @@
 class DatasetFileSchemasController < ApplicationController
 
   def index
-    @dataset_file_schemas = DatasetFileSchema.where(user: current_user)#.paginate(page: params[:page], per_page: 7).order(name: :asc)
+    @dataset_file_schemas = DatasetFileSchema.where(user: current_user).order(created_at: :desc)
   end
 
   def new
