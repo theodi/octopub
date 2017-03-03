f1f3face1214e53b8081de5bcc5fae8d437bce6e
James Jefferies <james@shedcode.co.uk>
2017-03-03 13:52:21 +0000

    Add tests around showing dataset file schema

--- spec/features/logged_in_access_spec.rb
+++ spec/features/logged_in_access_spec.rb
@@ -26,33 +26,5 @@ feature "Logged in access to pages", type: :feature do
       expect(page).to have_content "You currently have no dataset file schemas"
     end
   end
-
-  scenario "logged in visitors can view their dataset file schemas" do
-
-    schema_name = 'Good schema name'
-    schema_description = 'Good schema description superduper'
-
-    good_schema_url = url_with_stubbed_get_for_fixture_file('schemas/good-schema.json')
-    DatasetFileSchemaService.new(schema_name, schema_description, good_schema_url, @user).create_dataset_file_schema
-    visit root_path
-
-    click_link "List my dataset file schemas"
-    expect(page).to have_content schema_name
-  end
-
-  scenario "logged in visitors cannot view other users dataset file schemas" do
-
-    other_user = create(:user, name: "Woof McUser", email: "woof@user.com")
-
-    schema_name = 'Good schema name'
-    schema_description = 'Good schema description superduper'
-
-    good_schema_url = url_with_stubbed_get_for_fixture_file('schemas/good-schema.json')
-    create(:dataset_file_schema, url_in_repo: good_schema_url, name: schema_name, description: schema_description, user: other_user)
-    visit root_path
-
-    click_link "List my dataset file schemas"
-    expect(page).to have_content "You currently have no dataset file schemas"
-  end
 end
 
