bec87a8d437727cc7c4a742d5ceb87eff5ed61bd
James Jefferies <james@shedcode.co.uk>
2017-03-05 11:39:45 +0000

    Add new frequency

--- config/initializers/licenses_and_frequencies.rb
+++ config/initializers/licenses_and_frequencies.rb
@@ -1,5 +1,5 @@
 module Octopub
   API_LICENCES = ["CC-BY-4.0", "CC-BY-SA-4.0", "CC0-1.0", "OGL-UK-3.0", "ODC-BY-1.0", "ODC-PDDL-1.0"]
   WEB_LICENCES = ["cc-by", "cc-by-sa", "cc0", "OGL-UK-3.0", "odc-by", "odc-pddl"]
-  PUBLICATION_FREQUENCIES = ['One-off', 'Annual', 'Every working day', 'Daily', 'Monthly', 'Every minute', 'Every quarter', 'Half yearly', 'Weekly']  
+  PUBLICATION_FREQUENCIES = ['One-off', 'Annual', 'Every working day', 'Daily', 'Monthly', 'Bi-monthly', 'Every minute', 'Every quarter', 'Half yearly', 'Weekly']  
 end
