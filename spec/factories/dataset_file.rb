6943870eca97e4a978b57e47a205d0a1374ef49f
James Jefferies <james@shedcode.co.uk>
2017-03-05 20:58:19 +0000

    Adding tests for editing datasets

--- spec/factories/dataset_file.rb
+++ spec/factories/dataset_file.rb
@@ -1,3 +1,8 @@
+# require_relative "../features/user_and_organisations.rb"
+# require_relative "../spec_helper.rb"
+# require_relative "../rails_helper.rb"
+require 'webmock/rspec'
+
 FactoryGirl.define do
   factory :dataset_file do
     title 'My Awesome Dataset'
@@ -18,6 +23,16 @@ FactoryGirl.define do
       }
     end
 
-
+    # factory :dataset_file_with_schema do
+    #   file Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'fixtures', 'valid-schema.csv'), "text/csv")
+    #   dataset_file_schema { create(:dataset_file_schema, url_in_s3:  Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'fixtures', 'schemas/good-schema.json'), "text/json")) }
+    # end
   end
 end
+
+# def url_with_stubbed_get_for_fixture_file(file_name)
+#   path = File.join(Rails.root, 'spec', 'fixtures', file_name)
+#   url = "https://example.org/uploads/#{SecureRandom.uuid}/somefile.csv"
+#   stub_request(:get, url).to_return(body: File.read(path))
+#   url
+# end
