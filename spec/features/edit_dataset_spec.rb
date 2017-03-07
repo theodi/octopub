8c64c974438b0e35b90c63ff2897e8d5f4c79d8d
James Jefferies <james@shedcode.co.uk>
2017-03-07 21:05:28 +0000

    Fix the failing test by making sure the test Dataset is created for the right user!

--- spec/features/edit_dataset_spec.rb
+++ spec/features/edit_dataset_spec.rb
@@ -14,7 +14,7 @@ feature "Add dataset page", type: :feature, vcr: { :match_requests_on => [:host,
     skip_callback_if_exists(Dataset, :create, :after, :create_repo_and_populate)
     good_schema_url = url_with_stubbed_get_for_fixture_file('schemas/good-schema.json')
     create(:dataset_file_schema, url_in_repo: good_schema_url, name: 'good schema', description: 'good schema description', user: @user)
-    @dataset = create(:dataset, name: dataset_name)
+    @dataset = create(:dataset, name: dataset_name, user: @user)
     file = create(:dataset_file, dataset_file_schema: @dataset_file_schema,
                                   filename: "example.csv",
                                   title: dataset_file_name,
@@ -32,7 +32,7 @@ feature "Add dataset page", type: :feature, vcr: { :match_requests_on => [:host,
 
   context "logged in visitor has datasets and" do
     scenario "can view them" do
-      visit datasets_path
+      visit dashboard_path
       expect(page).to have_content dataset_name
       expect(page).to have_content 'Edit'
     end
