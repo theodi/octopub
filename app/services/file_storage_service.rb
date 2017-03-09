73a7c37b6fc6ebfb5e5b5f3de29c0e571aea6951
James Jefferies <james@shedcode.co.uk>
2017-03-09 17:29:06 +0000

    Add methods for public and private adding

--- app/services/file_storage_service.rb
+++ app/services/file_storage_service.rb
@@ -16,4 +16,29 @@ class FileStorageService
     S3_BUCKET
   end
 
+  def self.create_and_upload_public_object(filename, body)
+    key = object_key(filename)
+    obj = get_bucket.object(key)
+    url = URI.parse(obj.presigned_url(:put, acl: 'public-read'))
+
+    Net::HTTP.start(url.host) do |http|
+      http.send_request("PUT", url.request_uri, body, {
+        # This is required, or Net::HTTP will add a default unsigned content-type.
+        "content-type" => "",
+      })
+    end
+    obj
+  end
+
+  def self.create_and_upload_private_object(filename, body)
+    key = object_key(filename)
+    obj = S3_BUCKET.object(key)
+    obj.put(body: body)
+    obj
+  end
+
+  def self.object_key(filename)
+    "uploads/#{SecureRandom.uuid}/#{filename}"
+  end
+
 end
