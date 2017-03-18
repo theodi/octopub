b1bd47ec46e3fb37a5bb61020f437dc7e3588cc8
James Jefferies <james@shedcode.co.uk>
2017-03-18 16:48:36 +0000

    Re-do navigation with non-JS friendly drop downs and ensure users are logged in when they should be! Make some more menu space for new features

--- spec/features/public_access_spec.rb
+++ spec/features/public_access_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-feature "Public access to pages", type: :feature do
+feature "A visiting, non-logged in user", type: :feature do
 
   before(:each) do
     visit root_path
@@ -8,28 +8,60 @@ feature "Public access to pages", type: :feature do
     expect(page).to have_content "Sign in with Github"
   end
 
-  scenario "visitor can access the home page and datasets" do
-    click_link "All datasets"
-    expect(page).to have_content "You currently have no datasets"
+  context "can access the home page" do
+    scenario "and datasets" do
+      click_link "All datasets"
+      expect(page).to have_content "You currently have no datasets"
+    end
+
+    scenario "and can see a list of datasets when there are some" do
+      FactoryGirl.create(:dataset, name: 'Woof', url: 'https://meow.com', owner_avatar: 'https://meow.com')
+      click_link "All datasets"
+      expect(page).to have_content "Woof"
+    end
+
+
   end
-
-  scenario "visitor can access the home page, API page and can see a list of datasets" do
-    FactoryGirl.create(:dataset, name: 'Woof', url: 'https://meow.com', owner_avatar: 'https://meow.com')
-    click_link "All datasets"
-
-    expect(page).to have_content "Woof"
-  end
-
 
   # scenario "visitor can access the home page API page" do
   #   # requires Javascript capybara...
   #   pending "awaiting set up for Javascript"
-
   #   # visit root_path
-
   #   # click_link "API"
   #   # wait_for_ajax
-
   #   # expect(page).to have_content 'Octopub API'
   # end
+
+  context "can not access" do
+
+    after(:each) do
+      expect(page).to have_content "You must be logged in to do that"
+    end
+
+    it "new dataset path" do 
+      visit new_dataset_path 
+    end
+    it "edit dataset path" do 
+      visit edit_dataset_path(1)
+    end
+    it "new dashboard path" do 
+      visit dashboard_path 
+    end
+    it "me path" do 
+      visit me_path 
+    end
+    it "new dataset file schema path" do 
+      visit new_dataset_file_schema_path 
+    end
+  end 
+
+  context "can not access - forbidden" do
+    after(:each) do
+      expect(page).to have_content "You do not have permission to view that page or resource"
+    end
+    it "users path" do 
+      visit users_path 
+    end
+  end 
+
 end
