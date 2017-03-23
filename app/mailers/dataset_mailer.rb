63a23fe552ae569393d4c88bf1778225112cdf8f
James Jefferies <james@shedcode.co.uk>
2017-03-23 16:10:57 +0000

    Update mailers for the different publishing methods

--- app/mailers/dataset_mailer.rb
+++ app/mailers/dataset_mailer.rb
@@ -4,8 +4,38 @@ class DatasetMailer < ActionMailer::Base
   def success(dataset)
     @user = dataset.user
     @dataset = dataset
-    mail(to: @user.email, subject: 'Your Octopub dataset has been created')
+    if @dataset.github_public?
+      template = 'success'
+    elsif @dataset.github_private?
+      template = 'success_private_github'
+    else
+      template = 'success_private_local'
+    end
+    mail(to: @user.email, subject: 'Your Octopub dataset has been created') do |format|
+      format.html { render template }
+    end
+
   end
+
+  # private
+
+  # def success_public_github
+  #   mail(to: @user.email, subject: 'Your Octopub dataset has been created') do |format|
+  #     format.html { render :success }
+  #   end
+  # end
+
+  # def success_private_github
+  #   mail(to: @user.email, subject: 'Your Octopub dataset has been created') do |format|
+  #     format.html { render :success_private_github }
+  #   end
+  # end
+
+  # def success_private_local
+  #   mail(to: @user.email, subject: 'Your Octopub dataset has been created') do |format|
+  #     format.html { render :success_private_local }
+  #   end
+  # end
 
   def error(dataset)
     @user = dataset.user
