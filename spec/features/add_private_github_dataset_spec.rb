4e40090cf9738e55327a208f9ba0be481034f26b
James Jefferies <james@shedcode.co.uk>
2017-03-23 09:21:05 +0000

    REfactor and tidy up these two

--- spec/features/add_private_github_dataset_spec.rb
+++ spec/features/add_private_github_dataset_spec.rb
@@ -24,38 +24,34 @@ feature "Add private github dataset page", type: :feature do
     Sidekiq::Testing.fake!
   end
 
-  context "logged in visitors has no schemas" do
-    scenario "and can complete a simple dataset form without adding a schema" do
-      click_link "Add dataset"
+  it "by completing a simple dataset form without adding a schema" do
+    click_link "Add dataset"
 
-      expect_any_instance_of(JekyllService).to receive(:create_data_files) { nil }
-      expect_any_instance_of(JekyllService).to receive(:push_to_github) { nil }
-      expect_any_instance_of(JekyllService).to_not receive(:create_public_views) { nil }
-      expect_any_instance_of(Dataset).to receive(:send_success_email) { nil }
+    expect_any_instance_of(JekyllService).to receive(:create_data_files) { nil }
+    expect_any_instance_of(JekyllService).to receive(:push_to_github) { nil }
+    expect_any_instance_of(JekyllService).to_not receive(:create_public_views) { nil }
+    expect_any_instance_of(Dataset).to receive(:send_success_email) { nil }
 
-      common_name = 'Fri1437'
+    common_name = 'Fri1437'
 
-      before_datasets = Dataset.count
-      expect(page).to have_selector(:link_or_button, "Submit")
-      within 'form' do
-        expect(page).to have_content @user.github_username
-        expect(page).to have_content "Upload a schema for this Data File"
-        complete_form(page, common_name, data_file)
-      end
-
-      click_on 'Submit'
-
-      expect(page).to have_content "Your dataset has been queued for creation, and you should receive an email with a link to your dataset on Github shortly."
-      expect(Dataset.count).to be before_datasets + 1
-      expect(Dataset.last.name).to eq "#{common_name}-name"
-      expect(Dataset.last.github_private?).to be true
+    before_datasets = Dataset.count
+    expect(page).to have_selector(:link_or_button, "Submit")
+    within 'form' do
+      expect(page).to have_content @user.github_username
+      expect(page).to have_content "Upload a schema for this Data File"
+      complete_form(page, common_name, data_file)
     end
+
+    click_on 'Submit'
+
+    expect(page).to have_content "Your dataset has been queued for creation, and you should receive an email with a link to your dataset on Github shortly."
+    expect(Dataset.count).to be before_datasets + 1
+    expect(Dataset.last.name).to eq "#{common_name}-name"
+    expect(Dataset.last.github_private?).to be true
   end
 
   def complete_form(page, common_name, data_file, owner = nil, licence = nil)
-
     dataset_name = "#{common_name}-name"
-
     fill_in 'dataset[name]', with: dataset_name
     fill_in 'dataset[description]', with: "#{common_name}-description"
     fill_in 'dataset[publisher_name]', with: "#{common_name}-publisher-name"
