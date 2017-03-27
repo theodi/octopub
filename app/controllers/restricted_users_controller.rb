b70a3987ab1a1df03d8cb837f03984d726d65515
James Jefferies <james@shedcode.co.uk>
2017-03-27 10:18:53 +0100

    Sorting out allocated categories to a user

--- app/controllers/restricted_users_controller.rb
+++ app/controllers/restricted_users_controller.rb
@@ -10,13 +10,29 @@ class RestrictedUsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
+    allocate_schemas
     @user.update(user_params)
     redirect_to users_path, notice: "User details updated"
   end
 
   private
 
+  def allocate_schemas
+    if params[:user].key?(:schema_category_ids)
+      schema_category_ids = params[:user][:schema_category_ids]
+
+      schema_categories = SchemaCategory.find(schema_category_ids)
+      dataset_file_schema_ids = schema_categories.map { |sc| sc.dataset_file_schemas.pluck(:id) }.flatten.uniq
+
+      current_allocated_schemas = params[:user][:allocated_dataset_file_schema_ids] ||= []
+
+      params[:user][:allocated_dataset_file_schema_ids] = current_allocated_schemas + dataset_file_schema_ids
+      params[:user].delete(:schema_category_ids)
+    end
+
+  end
+
   def user_params
-    params.require(:user).permit(:email, :twitter_handle, :role, :restricted, allocated_dataset_file_schema_ids: [] )
+    params.require(:user).permit(:email, :twitter_handle, :role, :restricted, allocated_dataset_file_schema_ids: [], schema_category_ids: [])
   end
 end
