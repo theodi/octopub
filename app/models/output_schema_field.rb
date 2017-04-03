1613afd12de2787a2dd2e119ca1a71e0ded28e16
James Jefferies <james@shedcode.co.uk>
2017-04-03 17:13:50 +0100

    Add friendlies as constant

--- app/models/output_schema_field.rb
+++ app/models/output_schema_field.rb
@@ -5,8 +5,9 @@ class OutputSchemaField < ApplicationRecord
   # Of course, sum, group_by and names like that break in rails
   enum aggregation_type: [:ignoring, :totaling, :grouping]
 
+  FRIENDLY_HASH = { ignoring: 'Ignore this field', totaling: 'Sum this field', grouping: 'Group by this field' }
+
   def self.friendly_aggregation_types
-    friendly_hash = { ignoring: 'Ignore this field', totaling: 'Sum this field', grouping: 'Group by this field' }
-    aggregation_types.map {|type_name, _type_integer| [friendly_hash[type_name.to_sym], type_name]}
+    aggregation_types.map {|type_name, _type_integer| [FRIENDLY_HASH[type_name.to_sym], type_name]}
   end
 end
