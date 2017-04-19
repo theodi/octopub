6b826b6600794b0913937818b1c64f281aaa652f
Stephen Fortune <stephen.fortune@theodi.org>
2017-04-19 12:13:41 +0100

    Add URL tracking field to dataset table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403143913) do
+ActiveRecord::Schema.define(version: 20170419104036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20170403143913) do
     t.string   "certificate_url"
     t.string   "job_id"
     t.integer  "publishing_method", default: 0, null: false
+    t.boolean  "url_found"
     t.index ["user_id"], name: "index_datasets_on_user_id", using: :btree
   end
 
