3f79bd4ab16df50b226581f02350fcdd3f698061
Stephen Fortune <stephen.fortune@theodi.org>
2017-04-26 12:22:12 +0100

    Include JS to hover tooltip

---
+++ app/assets/javascripts/tooltips.js
@@ -0,0 +1,8 @@
+/**
+ * Created by stephenfortune on 26/04/2017.
+ */
+"use strict";
+$(document).ready(function(){
+    console.log("LOADED FILE!!!")
+    $('[data-toggle="tooltip"]').tooltip();
+});
