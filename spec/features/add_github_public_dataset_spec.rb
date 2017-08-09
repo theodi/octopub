c3c6ffcd355f4ef03ab892df32b78cd76cb9d6b6
James Smith <james@floppy.org.uk>
2017-08-09 18:22:40 +0100

    Make schemas public in dataset uploader form
    I can’t work out how the hell to write a test for this. Nothing I try works.

--- spec/features/add_github_public_dataset_spec.rb
+++ spec/features/add_github_public_dataset_spec.rb
@@ -7,6 +7,7 @@ feature "Add dataset page", type: :feature do
   include_context 'odlifier licence mock'
 
   let(:data_file) { get_fixture_file('valid-schema.csv') }
+  let(:schema_file) { get_fixture_file('schemas/good-schema.json') }
 
   before(:each) do
     allow_any_instance_of(DatasetsController).to receive(:current_user) { @user }
@@ -80,6 +81,62 @@ feature "Add dataset page", type: :feature do
       expect(Dataset.last.name).to eq "#{common_name}-name"
       expect(Dataset.last.owner).to eq @user.github_username
     end
+
+    context "and while creating a dataset, can upload a schema" do
+      
+      before :each do
+        # repo = double(GitData)
+        # expect(repo).to receive(:html_url) { 'https://example.org' }
+        # expect(repo).to receive(:name) { 'examplename'}
+        # expect(repo).to receive(:full_name) { 'examplename' }
+        # expect(RepoService).to receive(:create_repo) { repo }
+        # expect(RepoService).to receive(:fetch_repo).at_least(:once) { repo }
+        # expect(RepoService).to receive(:prepare_repo).at_least(:once)
+        # allow(DatasetFileSchemaService).to receive(:read_file_with_utf_8).and_return(read_fixture_schema_file('good-schema.json'))
+        # allow(DatasetFile).to receive(:read_file_with_utf_8).and_return(File.read(data_file))
+        # expect_any_instance_of(JekyllService).to receive(:create_data_files) { nil }
+        # expect_any_instance_of(JekyllService).to receive(:push_to_github) { nil }
+        # expect_any_instance_of(Dataset).to receive(:publish_public_views) { nil }
+        # expect_any_instance_of(Dataset).to receive(:send_success_email) { nil }
+      end
+      
+      scenario "privately" do
+        pending "can't work out proper mocking"
+
+        expect(DatasetFileSchema.count). to eq 0
+        # click_link "Add dataset"
+        # within 'form' do
+        #   complete_form(page, "ABC123", data_file)
+        #   fill_in 'files[][schema_name]', with: "ABC123-schema"
+        #   attach_file("[files[][schema]]", schema_file) # doesn't work right
+        # end
+        # click_on 'Submit'
+        
+        expect(DatasetFileSchema.count).to eq 1
+        expect(DatasetFileSchema.first.name).to eql "ABC123-schema"
+        expect(DatasetFileSchema.first.restricted).to eql true
+      end
+      
+      scenario "publicly" do
+        pending "can't work out proper mocking"
+
+        expect(DatasetFileSchema.count). to eq 0
+        # click_link "Add dataset"
+        # within 'form' do
+        #   complete_form(page, "ABC123", data_file)
+        #   fill_in 'files[][schema_name]', with: "ABC123-schema"
+        #   attach_file("[files[][schema]]", schema_file) # doesn't work right
+        #   select "Public - any user may access this schema", from: "[files[][schema_restricted]]"
+        # end
+        # click_on 'Submit'
+        
+        expect(DatasetFileSchema.count).to eq 1
+        expect(DatasetFileSchema.first.name).to eql "ABC123-schema"
+        expect(DatasetFileSchema.first.restricted).to eql false
+      end
+      
+    end
+
   end
 
   def complete_form(page, common_name, data_file, owner = nil, licence = nil)
