c621e57b6458ac7270bc6cd9ec7720a3fe354e1f
James Smith <james@floppy.org.uk>
2017-09-13 09:03:44 +0100

    don’t need to provide the host in the form, that’s an awful idea

--- app/assets/javascripts/file_upload.js
+++ app/assets/javascripts/file_upload.js
@@ -39,12 +39,9 @@ function bgUpload(elem) {
       progressBar.removeClass('active');
       // extract key and generate URL from response
       var key    = $(data.jqXHR.responseXML).find("Key").text();
-      var s3Host = form.data('host'); 
 
       // create hidden field
       var input = $("<input />", { type:'hidden', name: fileInput.attr('name'), value: key, class: 's3-file' });
-      container.append(input);
-      input = $("<input />", { type:'hidden', name: '[files[][schema_s3_host]]', value: s3Host, class: 's3-host' });
       container.append(input);
     },
     fail: function(e, data) {
