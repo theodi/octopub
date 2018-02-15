1000f6fd9ca34db272240777ec4f204bfd985d8b
Phil Lang <plangphoto@gmail.com>
2018-02-15 09:42:14 +0000

    Dashboard - search input linked up with table, styling applied to search input

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -2,6 +2,13 @@
 
 $(document).ready(function() {
 
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+
   if ($('#refresh').length) {
     var pusher = setUpPusher();
 
