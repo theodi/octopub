065854cc1d261503580c9ca8a986d6731f29d389
Phil Lang <plangphoto@gmail.com>
2018-03-19 10:43:01 +0000

    Update licencing copy - fix

--- config/initializers/licenses_and_frequencies.rb
+++ config/initializers/licenses_and_frequencies.rb
@@ -1,6 +1,6 @@
 module Octopub
   API_LICENCES = ["CC-BY-4.0", "CC-BY-SA-4.0", "CC0-1.0", "OGL-UK-3.0", "ODC-BY-1.0", "ODC-PDDL-1.0"]
-	BIG_LICENCES = ["CC BY - 4.0 (Creative Commons Attribution 4.0)", "CC BY SA - 4.0 (Creative Commons Attribution ShareAlike 4.0)",
+	BIG_LICENCES = ["CC BY - 4.0 (Creative Commons Attribution 4.0)", "CC BY SA - 4.0 (Creative Commons Attribution Share Alike 4.0)",
 									"CC0 - 1.0 (Creative Commons No Rights Reserved)", "OGL UK - 3.0 (Open Government Licence - Use & Re-use 3.0)",
 									"ODC BY - 1.0 (Open Data Commons Attribution License 1.0)", "ODC PDDL - 1.0 (ODC Public Domain Dedication & Licence 1.0)"]
   WEB_LICENCES = ["cc-by", "cc-by-sa", "cc0", "OGL-UK-3.0", "odc-by", "odc-pddl"]
