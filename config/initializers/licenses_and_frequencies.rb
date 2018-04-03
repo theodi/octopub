e7a224bab10857f93fa59fb5196768463cc5bcc6
Cai Williamson <caiwilliamson@users.noreply.github.com>
2018-04-03 14:42:06 +0100

    updated a license id that doesn't mmatch the id on opendefinition.org

--- config/initializers/licenses_and_frequencies.rb
+++ config/initializers/licenses_and_frequencies.rb
@@ -3,7 +3,7 @@ module Octopub
 	BIG_LICENCES = ["CC BY - 4.0 (Creative Commons Attribution 4.0)", "CC BY SA - 4.0 (Creative Commons Attribution Share Alike 4.0)",
 									"CC0 - 1.0 (Creative Commons No Rights Reserved)", "OGL UK - 3.0 (Open Government Licence - Use & Re-use 3.0)",
 									"ODC BY - 1.0 (Open Data Commons Attribution License 1.0)", "ODC PDDL - 1.0 (ODC Public Domain Dedication & Licence 1.0)"]
-  WEB_LICENCES = ["cc-by", "cc-by-sa", "cc0", "OGL-UK-3.0", "odc-by", "odc-pddl"]
+  WEB_LICENCES = ["cc-by", "cc-by-sa", "cc0", "OGL-UK-3.0", "odc-by", "ODbL-1.0"]
 	URL_LICENCES = ["https://creativecommons.org/licenses/by/4.0/", "https://creativecommons.org/licenses/by-sa/4.0/",
 									"https://creativecommons.org/share-your-work/public-domain/cc0", "http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/",
 									"https://opendatacommons.org/licenses/by/1-0/", "https://opendatacommons.org/licenses/pddl/1-0/"]
