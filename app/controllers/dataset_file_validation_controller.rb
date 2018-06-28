bb9603d18136f0b45a1ce455d15fd3340f9e9461
Phil Lang <plangphoto@gmail.com>
2018-06-28 12:09:38 +0100

    Add basic validation page for dataset files

--- app/controllers/dataset_file_validation_controller.rb
+++ app/controllers/dataset_file_validation_controller.rb
@@ -3,7 +3,7 @@ class DatasetFileValidationController < ApplicationController
 	def index
 		dataset_file_id = params[:dataset_file_id]
 		dataset_file = DatasetFile.find(dataset_file_id)
-		@results = CsvlintValidateService.validate_single_csv(dataset_file)
+		@result = CsvlintValidateService.validate_single_csv(dataset_file)
 	end
 
 end
