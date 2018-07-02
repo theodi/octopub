606554299c744a37866fcd219bcb7e808a85d726
Phil Lang <plangphoto@gmail.com>
2018-07-02 16:25:11 +0100

    Add data set file title to validation view

--- app/controllers/dataset_file_validation_controller.rb
+++ app/controllers/dataset_file_validation_controller.rb
@@ -2,8 +2,8 @@ class DatasetFileValidationController < ApplicationController
 
 	def index
 		dataset_file_id = params[:dataset_file_id]
-		dataset_file = DatasetFile.find(dataset_file_id)
-		@result = CsvlintValidateService.get_validated_csv(dataset_file)
+		@dataset_file = DatasetFile.find(dataset_file_id)
+		@result = CsvlintValidateService.get_validated_csv(@dataset_file)
 	end
 
 end
