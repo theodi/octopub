195c97b94461b18c1a3d53d37be2e65e0fb58238
Phil Lang <plangphoto@gmail.com>
2018-07-17 15:22:34 +0100

    Schema being passed to CSVlint - needs refactoring and error guarding

--- app/services/csvlint_validate_service.rb
+++ app/services/csvlint_validate_service.rb
@@ -3,18 +3,27 @@ class CsvlintValidateService
 	require 'csvlint'
 
 	def self.validate_csv(file)
-		csv_string = get_s3_string(file)
-		validator = lint_csv(csv_string)
+		csv = get_s3_string(file)
+		# schema = Csvlint::Schema.load_from_string(URI.escape(file.dataset_file_schema.url), file.dataset_file_schema.schema)
+		# tempfile = Tempfile.new 'uploaded'
+		# tempcsv = File.new(file.tempfile)
+
+		# The schema object below is correct for passing to validator
+		schema = file.dataset_file_schema.parsed_schema
+		# file_validation = Csvlint::Validator.new(csv, {}, schema)
+
+		validator = lint_csv(csv, schema)
 		update_database_attributes(validator, file)
 	end
 
 	def self.get_validated_csv(file)
 		csv_string = get_s3_string(file)
-		lint_csv(csv_string)
+		schema = file.dataset_file_schema.parsed_schema
+		lint_csv(csv_string, schema)
 	end
 
-	def self.lint_csv(csv_string)
-		return Csvlint::Validator.new(csv_string)
+	def self.lint_csv(csv, schema)
+		return Csvlint::Validator.new(csv, {}, schema)
 	end
 
 	def self.generate_badge(file)
@@ -33,7 +42,8 @@ class CsvlintValidateService
 
 	def self.generate_badge_valid_file(file)
 		csv_string = get_s3_string(file)
-		csv = lint_csv(csv_string)
+		schema = file.dataset_file_schema.parsed_schema
+		csv = lint_csv(csv_string, schema)
 
 		csv.warnings.count > 0 ? "warnings" : "valid"
 	end
