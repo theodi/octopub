0374f56e667574f64c1d4f25a23f290c6bd51117
Phil Lang <plangphoto@gmail.com>
2018-07-20 16:03:17 +0100

    Add anchor links to signed out home page - getting started guidance

--- app/assets/javascripts/getting-started.js
+++ app/assets/javascripts/getting-started.js
@@ -1,10 +1,10 @@
-function addAccordionListeners()
-{
-    var acc = document.getElementsByClassName("accordion");
+function addAccordionListeners() {
+    var opener = document.getElementsByClassName("opener");
     var i;
 
-    for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function () {
+    for (i = 0; i < opener.length; i++) {
+        opener[i].addEventListener("click", function () {
+					console.log(this.classList)
             this.classList.toggle("active");
             var panel = this.nextElementSibling;
             if (panel.style.maxHeight) {
@@ -14,4 +14,21 @@ function addAccordionListeners()
             }
         });
     }
+}
+
+function anchorTagOpener() {
+	$(document).ready(function() {
+		let hash = window.location.hash.replace('#','');
+		let opener = document.getElementsByClassName(hash)[0];
+		var panel = opener.nextElementSibling;
+
+		opener.classList.toggle('active')
+
+		if (panel.style.maxHeight) {
+				panel.style.maxHeight = null;
+		} else {
+				panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+		$(window).scrollTop($("." + hash).offset().top)
+	})
 }
